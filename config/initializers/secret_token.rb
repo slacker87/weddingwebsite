1fe2fdf980e455ecf1ea83ba61134f1e7c45ba55
slacker87 <racethesunlive@yahoo.com>
2013-10-23 20:56:29 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swiftrickertwedding::Application.config.secret_key_base = '73fd145d9e72b1017fc009a26ba4e7e852e9e5cd7744fa1d1e144c506ce1ad1c7ab35a85e88b54eafbeabd9fda18af60b153b40c0731723633839a2089da3323'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swiftrickertwedding::Application.config.session_store :cookie_store, key: '_swiftrickertwedding_session'

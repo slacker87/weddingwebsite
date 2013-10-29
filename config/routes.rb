a8cefaf08fd2d8cbf656a8d4894e3afcd21cbcaa
slacker87 <racethesunlive@yahoo.com>
2013-10-28 22:24:07 -0400

    remove menu page, add photos page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ root  to: 'static_pages#home'
 
 match '/help',  to: 'static_pages#help', via: 'get'
 match '/date', to: 'static_pages#date', via: 'get'
-match '/menu', to: 'static_pages#menu', via: 'get'
 match '/venue', to: 'static_pages#venue', via: 'get'
 match '/contact', to: 'static_pages#contact', via: 'get'
+match '/photos', to: 'static_pages#photos', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

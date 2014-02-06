890e37a5a12bc23d6c3e134bf424e307ba20eae2
slacker87 <racethesunlive@yahoo.com>
2014-02-06 17:53:34 +0000

    update site with new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Swiftrickertwedding::Application.routes.draw do
 
-root  to: 'static_pages#home'
+  root  to: 'static_pages#index'
 
 match '/help',  to: 'static_pages#help', via: 'get'
 match '/date', to: 'static_pages#date', via: 'get'
